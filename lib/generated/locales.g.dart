9a16d918e83707fdefe1b90f1d607a862600348c
liang baoxian <liangbx361@163.com>
2024-02-04 09:50:26 +0800

    refactor: 新增本地化文本

---
+++ lib/generated/locales.g.dart
@@ -0,0 +1,28 @@
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+// ignore_for_file: lines_longer_than_80_chars
+// ignore: avoid_classes_with_only_static_members
+class AppTranslation {
+  static Map<String, Map<String, String>> translations = {
+    'zh_CN': Locales.zh_CN,
+    'en': Locales.en,
+    'zh_TW': Locales.zh_TW,
+  };
+}
+
+class LocaleKeys {
+  LocaleKeys._();
+  static const hello = 'hello';
+}
+
+class Locales {
+  static const zh_CN = {
+    'hello': '你好，简体世界！',
+  };
+  static const en = {
+    'hello': 'Hello, English World!',
+  };
+  static const zh_TW = {
+    'hello': '你好，繁体世界！',
+  };
+}
